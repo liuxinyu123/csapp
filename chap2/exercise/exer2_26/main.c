8f65c1f6d8d67f9b7ac88890709e3dcd9bd92b24
liuyakun <happy_lucky52@163.com>
2017-04-01 19:54:02 +0800

    exer2_26

---
+++ chap2/exercise/exer2_26/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int str_longer (char *s, char *t);
+
+int main (int argc, char *argv[])
+{
+	char a[] = "hello";
+	char b[] = "worlddd";
+
+	printf ("%d\n", str_longer (a, b));
+
+	return 0;
+}
+
+int str_longer (char *s, char *t)
+{
+	return strlen (s) - strlen (t) > 0;
+}
+
+
